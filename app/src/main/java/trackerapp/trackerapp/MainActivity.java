9981c00988ff5597f66faf4fb12497fcd21a17ef
Krystian Krzywda <krystiankrzywda22@gmail.com>
2017-05-04 21:42:45 +0200

    create LoginActivity, set MainActivity as luncher

--- app/src/main/java/trackerapp/trackerapp/MainActivity.java
+++ app/src/main/java/trackerapp/trackerapp/MainActivity.java
@@ -1,25 +1,56 @@
 package trackerapp.trackerapp;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.view.View;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+
+    private MainActivity self;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        self = this;
+
+
+        checkAccess(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(self, TrackerActivity.class);
+                self.finish();
+                self.startActivity(intent);
+            }
+        }, new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(self, LoginActivity.class);
+                self.finish();
+                self.startActivity(intent);
+            }
+        });
+
+    }
+
+    private void checkAccess(Runnable success, Runnable failed) {
+
+        // TODO: połączenie z api, ktore sprawdza czy jestesmy zalogowani
+        // jestesmy zalogowani
+       /* if(success != null) {
+            success.run();
+        }*/
+        // nie jestesmy zalogowani
+        if(failed != null) {
+            failed.run();
+        }
 
     }
 
